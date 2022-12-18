1dd30578c8bc6ab67e952c40816498ffe741a26d
Said Gadjiev <ackusha1984@ya.ru>
2022-12-19 00:24:58 +0300

    решение 43 задачи

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -6,3 +6,35 @@ y = k1 * x + b1, y = k2 * x + b2;
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
+
+double CorrectNumber(string messege)
+{
+    bool isCorrect = false;
+    double result = 0;
+    while (! isCorrect)
+    {
+        Console.Write(messege);
+        if (double.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else Console.Write("Введите корректное число -> ");
+    }
+    return result;
+}
+
+double b1 = CorrectNumber("Введите значение b1 -> ");
+double k1 = CorrectNumber("Введите значение k1 -> ");
+double b2 = CorrectNumber("Введите значение b2 -> ");
+double k2 = CorrectNumber("Введите значение k2 -> ");
+
+if (b1 == b2 && k1 == k2) Console.WriteLine("Две линии лежат друг на друга, все точки первой и второй линии сходятся");
+
+if (b1 != b2 && k1 == k2) Console.WriteLine("Две линии линии паралельны они не пересекаються");
+
+else
+{
+    double x = ((b2 - b1) / (k1 - k2));
+    double y = k1 * x + b1;
+    Console.WriteLine($"{x}, {y}");
+}
